0a6044d712c2e3bad17f801e9eb82466a354e24d
Shunji Lin <lgxshunji@gmail.com>
2018-02-13 15:07:45 +0900

    make async at probe level

--- src/astar_pidd.hpp
+++ src/astar_pidd.hpp
@@ -12,7 +12,7 @@
 #include "node.hpp"
 
 #include "compress/compress_open_list.hpp"
-#include "compress/compress_closed_list.hpp"
+#include "compress/compress_closed_list_async.hpp"
 #include "utils/compunits.hpp"
 //#include "utils/scoped_thread.hpp"
 
@@ -24,14 +24,29 @@
 using namespace compunits;
 using namespace compress;
 
-const std::size_t N_THREADS = std::thread::hardware_concurrency() - 1;
+const std::size_t N_THREADS = 32;//std::thread::hardware_concurrency() - 1;
 using FoundReopened = std::pair<bool, bool>;
 
 namespace astar_pidd {
 
+    template<class Domain>
+    struct NodeStats {
+        Node<Domain> node;
+        bool lookup;
+        bool found;
+        bool reopen;
+        bool first_probe;
+        size_t pointer;
+        NodeStats() : lookup(true), found(false), reopen(false),
+                      first_probe(true), pointer(0) {}
+        bool operator==(const NodeStats<Domain>& other) const {
+            return node == other.node;
+        }
+    };
+
     template<class Domain> class AStarPIDD : public SearchAlg<Domain> {
 
-        CompressClosedList<Node<Domain> > closed;
+        CompressClosedListAsync<Node<Domain> > closed;
         CompressOpenList<Node<Domain> > open;
     
         std::vector<typename Domain::State> path;
@@ -40,31 +55,21 @@ namespace astar_pidd {
 
         std::vector<Node<Domain> > collect_unique_nodes(std::size_t n_nodes) {
             // inefficient, could use a set here
+
+            //TODO: NEED TO ASSERT THAT LOWER G IS SELECTED
             std::vector<Node<Domain> > nodes;
             while (!open.isempty() &&
                    nodes.size() < n_nodes) {
                 Node<Domain> node = open.pop();
-                if (std::find(nodes.begin(), nodes.end(), node) == nodes.end()) {
-                    nodes.push_back(node); // only inserts unique nodes
-                } else {
+                auto it = std::find(nodes.begin(), nodes.end(), node);
+                if (it != nodes.end()) {
+                    if (it->g > node.g) it->g = node.g;
                     ++duplicates;
-                }
-                    
+                } else {
+                    nodes.push_back(node);
+                }   
             }
             return nodes;
-        }
-
-        std::vector<std::future<FoundReopened> >
-        batch_duplicate_detection(std::vector<Node<Domain> > &nodes) {
-         
-            std::vector<std::future<FoundReopened> > futures;
-            for (auto& node : nodes) {
-                futures.push_back
-                    (std::async(std::launch::async,
-                                &CompressClosedList<Node<Domain> >::find_in_closed,
-                                &this->closed, node));
-            }
-            return futures;
         }
 
     public:
@@ -83,40 +88,10 @@ namespace astar_pidd {
                 std::vector<Node<Domain> > reopened_nodes;
 
                 auto nodes = collect_unique_nodes(N_THREADS);
-
-                // check for duplicates in buffer
-                for (auto &node : nodes) {
-                    bool found, reopened;
-                    tie(found, reopened) = closed.find_in_buffers(node);
-                    if (found && reopened) {
-                        this->reopd++;
-                        reopened_nodes.push_back(node);
-                        node.g = -1; // remove node TODO: fix hack
-                    }
-                    if (found && !reopened) {
-                        node.g = -1; // remove node
-                    }
-                }
                 
-                
-                nodes.erase(std::remove_if(nodes.begin(), nodes.end(),
-                               [](Node<Domain>& node) {
-                                               return node.g == -1;
-                                           }), nodes.end());
-
-                 auto duplicate_detection_futures =
-                     batch_duplicate_detection(nodes);
-
-               
-                nodes.insert(nodes.end(), reopened_nodes.begin(), reopened_nodes.end());
-                
+                closed.batch_duplicate_detection(nodes);
+                               
                 for (std::size_t i = 0; i < nodes.size(); ++i) {
-                    if (i < duplicate_detection_futures.size()) {
-                        bool found, reopened;
-                        tie(found, reopened) = duplicate_detection_futures[i].get();
-                        if (found && reopened) this->reopd++;
-                        if (found && !reopened) continue;
-                    }
                     auto n = nodes[i];
                     
                     typename Domain::State state;
@@ -149,9 +124,6 @@ namespace astar_pidd {
                         open.push(wrap(state, &n, e.cost, e.pop));
                         this->dom.undo(state, e);
                     }
-                }
-                for (std::size_t i = 0; i < duplicate_detection_futures.size(); ++i) {
-                    closed.insert_in_buffer(nodes[i]);
                 }
             }
             return path;
---
+++ src/compress/compress_closed_list_async.hpp
@@ -0,0 +1,461 @@
+// Copyright 2017 Shunji Lin. All rights reserved.
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file.
+#ifndef COMPRESS_CLOSED_LIST_ASYNC_HPP
+#define COMPRESS_CLOSED_LIST_ASYNC_HPP
+
+#include "pointer_table.hpp"
+#include "mapping_table.hpp"
+#include "../utils/named_fstream.hpp"
+#include "../utils/memory.hpp"
+#include "../utils/errors.hpp"
+#include "../hash_functions/tabulation_hash.hpp"
+
+#include <iostream>
+#include <vector>
+#include <memory>
+#include <unordered_set>
+#include <cmath> // for pow
+
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#include <atomic>
+#include <future>
+#include <functional>
+
+using namespace std;
+
+using found = bool;
+using reopened = bool;
+
+namespace compress {
+    
+    template<class Entry>
+    class CompressClosedListAsync {
+        
+        struct EntryStats {
+            Entry entry;
+            bool valid = true;
+            bool first_probe = true;
+            size_t probe_index = 0;
+            size_t pointer = 0;
+            Entry external_entry;
+            EntryStats(Entry entry) : entry(entry) {}
+        };
+
+        bool reopen_closed;
+        bool enable_partitioning;
+        bool double_hashing;
+
+         TabulationHash<Entry> hasher;
+         TabulationHash<Entry> partition_hasher;
+
+        vector<unordered_set<Entry, decltype(hasher) > > buffers;
+
+        // for compress with partitioning
+        unique_ptr<MappingTable> partition_table;
+        unsigned n_partitions = 100;
+       
+        PointerTable internal_closed;
+        int external_closed_fd;
+        char *external_closed;
+        size_t external_closed_index = 0;
+        size_t external_closed_bytes = 0;
+
+        size_t max_buffer_size_in_bytes = BUFFER_BYTES;
+        size_t max_buffer_entries;
+
+        
+        unsigned get_partition_value(const Entry &entry) const;
+        
+        void flush_buffer(size_t partition_value);
+
+        void read_external_at(Entry& entry, size_t index) const;
+        void write_external_at(const Entry& entry, size_t index);
+
+        // probe statistics, does not include probes for path reconstruction
+        mutable size_t buffer_hits = 0;
+        mutable size_t good_probes = 0;
+        mutable size_t bad_probes = 0;
+
+        size_t get_probe_value(size_t hash_value) const;
+        
+    public:
+        explicit CompressClosedListAsync(bool reopen_closed,
+                                    bool enable_partitioning,
+                                    bool double_hashing,
+                                    size_t internal_closed_bytes);
+        
+        ~CompressClosedListAsync() = default;
+
+        int batch_duplicate_detection(std::vector<Entry>& entries);
+        
+        pair<found, reopened> find_in_buffers(const Entry &entry);
+        pair<found, reopened> find_in_closed(const Entry &entry);
+        void insert_in_buffer(const Entry &entry);
+
+        pair<found, reopened> find_insert(const Entry &entry);
+
+        Entry trace_parent(const Entry& entry) const;
+
+        void clear();
+        void print_statistics() const;
+
+    };
+    
+    template<class Entry>
+    CompressClosedListAsync<Entry>::CompressClosedListAsync(bool reopen_closed,
+                                                  bool enable_partitioning,
+                                                  bool double_hashing,
+                                                  size_t internal_closed_bytes)
+        : reopen_closed(reopen_closed),
+          enable_partitioning(enable_partitioning),
+          double_hashing(double_hashing),
+          internal_closed(internal_closed_bytes) 
+    {
+        max_buffer_entries = max_buffer_size_in_bytes / Entry::get_size_in_bytes();
+        
+        // initialize partition table
+        if (enable_partitioning) {
+            partition_table =
+                memory::make_unique<MappingTable>(max_buffer_entries);
+           
+            buffers.resize(n_partitions);
+        } else {
+            buffers.resize(1); // use only buffers[0] if no partitioning
+        }
+        
+        // initialize external closed list
+        external_closed_bytes =
+            internal_closed.get_max_entries() * Entry::get_size_in_bytes();
+        dfpair(stdout, "external closed (bytes)", "%lu", external_closed_bytes);
+
+        external_closed_fd = open("closed_list.bucket", O_CREAT | O_TRUNC | O_RDWR,
+                                  S_IRUSR | S_IWUSR);
+        if (external_closed_fd < 0)
+            throw IOException("Fail to create closed list file");
+        
+        if (posix_fallocate64(external_closed_fd, 0, external_closed_bytes) < 0)
+            throw IOException("Fail to fallocate closed list file");
+        
+        external_closed =
+            static_cast<char *>(mmap(NULL, external_closed_bytes,
+                                     PROT_READ | PROT_WRITE, MAP_SHARED,
+                                     external_closed_fd, 0));
+        if (external_closed == MAP_FAILED)
+            throw IOException("Fail to mmap closed list file");
+
+        if (madvise(external_closed, external_closed_bytes, MADV_RANDOM) < 0)
+            throw IOException("Fail to give madvise for closed list file");
+
+        // Logging
+        if (enable_partitioning)
+            dfpair(stdout, "number of partitions", "%u", n_partitions);
+        if (double_hashing) {
+            dfpair(stdout, "probe strategy", "%s", "double hashing");
+        } else {
+            dfpair(stdout, "probe strategy", "%s", "linear probing");
+        }
+        dfpair(stdout, "max capacity of closed list (nodes)", "%lu",
+               internal_closed.get_max_entries());
+    }
+
+    template<class Entry>
+    size_t CompressClosedListAsync<Entry>::get_probe_value(size_t hash_value) const {
+        if (!double_hashing) return 1; // linear probing
+        
+        // From Introduction to Algorithms 3rd Edition, pg 273
+        // This guarantees that double hashing does not cycle if max entries of
+        // internal_closed is prime.
+        return 1 + (hash_value % (internal_closed.get_max_entries() - 1));
+    }
+
+    template<class Entry>
+    pair<found, reopened> CompressClosedListAsync<Entry>::
+    find_in_buffers(const Entry &entry) {
+        auto partition_value = get_partition_value(entry);
+        
+        auto& buffer = buffers[partition_value];
+        
+        auto buffer_it = buffer.find(entry);
+        if (buffer_it != buffer.end()) {
+            ++buffer_hits;
+            if (reopen_closed) {
+                if (entry.g < buffer_it->g) {
+                    buffer.erase(buffer_it);
+                    buffer.insert(entry);
+                    return make_pair(true, true);
+                }
+            }
+            
+            return make_pair(true, false);
+        }
+        return make_pair(false, false);
+    }
+
+    // TODO: ensure this is thread safe
+    template<class Entry>
+    pair<found, reopened> CompressClosedListAsync<Entry>::
+    find_in_closed(const Entry &entry) {
+        auto partition_value = get_partition_value(entry);    
+        auto hash_value = hasher(entry);
+        auto probe_value = get_probe_value(hash_value);
+        auto ptr = internal_closed.get_ptr_with_hash(hash_value, probe_value);
+        while (!internal_closed.ptr_is_invalid(ptr)) {
+
+            // first check in partition table
+            if (!enable_partitioning ||
+                partition_value == partition_table->get_value_from_ptr(ptr)) {
+                // read node from pointer
+                Entry node;
+                read_external_at(node, ptr);
+                if (node == entry) {
+                    ++good_probes;
+                    if (reopen_closed) {
+                        if (entry.g < node.g) {
+                            write_external_at(entry, ptr);
+                            return make_pair(true, true);
+                        }
+                    }
+                    return make_pair(true, false);
+                }
+                ++bad_probes;
+            }
+            // update pointer and resume while loop if partition values do not
+            // match or if false positive probe
+            ptr = internal_closed.get_ptr_with_hash(hash_value, probe_value, false);
+        }
+        return make_pair(false, false);
+    }
+
+    template<class Entry>
+    void CompressClosedListAsync<Entry>::
+    insert_in_buffer(const Entry &entry) {
+        auto partition_value = get_partition_value(entry);
+        buffers[partition_value].insert(entry);
+        if (buffers[partition_value].size() == max_buffer_entries)
+            flush_buffer(partition_value);
+    }
+
+    template<class Entry>
+    pair<found, reopened> CompressClosedListAsync<Entry>::
+    find_insert(const Entry &entry) {
+        
+        // First look in buffer
+        auto found_reopened_in_buffers = find_in_buffers(entry);
+
+        if (found_reopened_in_buffers.first)
+            return found_reopened_in_buffers;
+        
+        // Then look in hash tables
+        auto found_reopened_in_closed = find_in_closed(entry);
+
+        if (found_reopened_in_closed.first)
+            return found_reopened_in_closed;
+        
+        insert_in_buffer(entry);
+        
+        return make_pair(false, false);
+    }
+
+    template<class Entry>
+    unsigned CompressClosedListAsync<Entry>::get_partition_value(const Entry& entry) const {
+        if (!enable_partitioning) return 0;
+        return partition_hasher(entry) % n_partitions;
+    }
+
+    template<class Entry>
+    void CompressClosedListAsync<Entry>::flush_buffer(size_t partition_value) {
+        //std::cout << " flush " << std::endl;
+        for (auto& node : buffers[partition_value]) {
+            write_external_at(node, external_closed_index);
+            auto hash_value = hasher(node);
+            internal_closed.insert_ptr_with_hash(external_closed_index,
+                                        hash_value,
+                                        get_probe_value(hash_value));
+            ++external_closed_index;
+        }
+        if (enable_partitioning)
+            partition_table->insert_map_value(partition_value);
+        // release memory
+        unordered_set<Entry, decltype(hasher) >().swap(buffers[partition_value]);
+    }
+    
+    
+    template<class Entry>
+    Entry CompressClosedListAsync<Entry>::
+    trace_parent(const Entry &entry) const {
+        
+        // first look in buffers
+        for (auto& buffer : buffers) {
+            for (auto& node: buffer) {
+                if (node.packed  == entry.parent_packed) {
+                    return node;
+                }
+            }
+        }
+        // Then look in hash tables
+        auto parent_hash_value = hasher(entry.parent_packed);
+        auto probe_value = get_probe_value(parent_hash_value);
+        auto ptr = internal_closed.get_ptr_with_hash(parent_hash_value, probe_value);
+        while (!internal_closed.ptr_is_invalid(ptr)) {
+            // read node from pointer
+            Entry node;
+            read_external_at(node, ptr);
+            if (node.packed == entry.parent_packed) {
+                return node;
+            }
+            // update pointer and resume while loop if partition values do not
+            // match or if false positive probe
+            ptr = internal_closed.get_ptr_with_hash(parent_hash_value,
+                                            probe_value,
+                                            false);
+        }
+        return Entry();
+    }
+
+    template<class Entry>
+    void CompressClosedListAsync<Entry>::
+    read_external_at(Entry& entry, size_t index) const {
+        entry.read(static_cast<char *>
+                   (external_closed + index * Entry::get_size_in_bytes()));
+    }
+
+    template<class Entry>
+    void CompressClosedListAsync<Entry>::
+    write_external_at(const Entry& entry, size_t index) {
+        entry.write(static_cast<char *>
+                    (external_closed + index * Entry::get_size_in_bytes()));
+    }
+
+    template<class Entry>
+    void CompressClosedListAsync<Entry>::clear() {
+        // Let errors go in clear() as we do not want termination at the end of
+        // search, and clean up of files is non-critical
+        munmap(external_closed, external_closed_bytes);
+        close(external_closed_fd);
+        remove("closed_list.bucket");
+    }
+    
+
+    template<class Entry>
+    void CompressClosedListAsync<Entry>::print_statistics() const {
+        dfpair(stdout, "size of node (bytes)", "%lu", Entry::get_size_in_bytes());
+        dfpair(stdout, "nodes in closed list",
+               "%lu", internal_closed.get_n_entries());
+        cout << "#pair  \"load factor\"   "
+             << "\"" << internal_closed.get_load_factor() << "\"" << endl; 
+        dfpair(stdout, "successful probes",
+               "%lu", good_probes);
+        dfpair(stdout, "usuccessful probes",
+               "%lu", bad_probes);
+        dfpair(stdout, "buffer hits",
+               "%lu", buffer_hits);
+        if (enable_partitioning) {
+            dfpair(stdout, "mapping table entries", "%lu",
+                   partition_table->size());
+            dfpair(stdout, "mapping table size (bytes)", "%lu",
+                   partition_table->get_size_in_bytes());
+        }
+    }
+
+    template<class Entry>
+    int CompressClosedListAsync<Entry>::
+    batch_duplicate_detection(std::vector<Entry>& entries) {
+        //std::cout << "enter " << std::endl;
+        // assume no reopenings TODO:fix this 
+        std::vector<Entry> to_expand;
+        std::vector<EntryStats> entries_stats;
+
+        int reopen = 0;
+
+        // check against buffers
+        for (auto& entry : entries) {
+            bool found, reopened;
+            tie(found, reopened) = find_in_buffers(entry);
+            /*(if (reopened) {
+                to_expand.emplace_back(entry);
+                ++reopen;
+                }*/
+            if (!found) {
+                entries_stats.push_back(EntryStats(entry));
+            }
+        }
+        
+        while (entries_stats.size() != 0) {
+            //std::cout << "entries size " << entries_stats.size() << std::endl;
+            // get pointers
+            for (auto& entry_stats : entries_stats) {
+                do {
+                    // update probe index
+                    if (entry_stats.first_probe) {
+                        entry_stats.probe_index =
+                            hasher(entry_stats.entry) % internal_closed.get_max_entries();
+                        entry_stats.first_probe = false;
+                    } else {
+                        entry_stats.probe_index =
+                            (entry_stats.probe_index +
+                             get_probe_value(hasher(entry_stats.entry)))
+                            % internal_closed.get_max_entries();
+                    }
+                    entry_stats.pointer =
+                        internal_closed.get_ptr_at_index(entry_stats.probe_index);
+                    if (internal_closed.ptr_is_invalid(entry_stats.pointer)) {
+                        to_expand.emplace_back(entry_stats.entry);
+                        entry_stats.valid = false;
+                    }
+                }
+                // if filtered by mapping table, update probe index
+                while (entry_stats.valid &&
+                       get_partition_value(entry_stats.entry) !=
+                       partition_table->get_value_from_ptr(entry_stats.pointer));
+                       
+            }
+            // remove unecessary entries
+            entries_stats.erase
+                (std::remove_if(entries_stats.begin(), entries_stats.end(),
+                                [](EntryStats& entrystats) {return !entrystats.valid;}),
+                 entries_stats.end());
+            
+            if (entries_stats.size() == 0) break; // nothing to probe
+            // check in external hash table
+            std::vector<std::future<void> > futures;
+            for (auto& entry_stats : entries_stats) {
+
+                futures.push_back(std::async(std::launch::async,
+                                             &CompressClosedListAsync<Entry>::read_external_at,
+                                             this,
+                                             std::ref(entry_stats.external_entry),
+                                             entry_stats.pointer));
+            }
+
+            for (size_t i = 0; i < entries_stats.size(); ++ i) {
+                futures[i].get();
+                  if (entries_stats[i].entry ==
+                      entries_stats[i].external_entry) {
+                      ++good_probes;
+                      entries_stats[i].valid = false;
+                  } else {
+                      ++bad_probes;
+                  }
+            }
+            
+            // remove unecessary entries
+            entries_stats.erase
+                (std::remove_if(entries_stats.begin(), entries_stats.end(),
+                                [](EntryStats& entrystats) {return !entrystats.valid;}),
+                 entries_stats.end());
+        }
+        for (auto& entry : to_expand) {
+            insert_in_buffer(entry);
+        }
+        entries.swap(to_expand);
+        return reopen;
+    }
+}
+
+#endif
